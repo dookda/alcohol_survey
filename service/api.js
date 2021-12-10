7e9ff28af4c698766259f050efdbdad0bda7db9c
dookda <sakda.homhuan@gmail.com>
2021-12-10 07:19:08 +0700

    edit report

--- service/api.js
+++ service/api.js
@@ -22,7 +22,8 @@ app.post("/alcohol-api/getdataone", (req, res) => {
 app.post("/alcohol-api/getdata", (req, res) => {
     const { usrid } = req.body;
 
-    const sql = `SELECT gid, pid, retail_name, owner_name, retail_type, product_type, 
+    const sql = `SELECT gid, pid, CONCAT(retail_name, owner_name, addresses, retail_type, product_type) as txt,
+            retail_name, owner_name, retail_type, product_type, 
             certification, addresses, retail_status, alcohol_survey, alcohol, 
             alcohol_item, cigarette_survey, cigarette, cigarette_item,lat,lng,
             TO_CHAR(ts, 'DD-MM-YYYY') as ndate, img, ST_AsGeojson(geom) as geojson  
--- www/report/app.js
+++ www/report/app.js
@@ -25,37 +25,50 @@ async function getUserid() {
 
 // var url = 'https://rti2dss.com:4000';
 var url = 'http://localhost:4000'
-
+let xdata = axios.post(url + '/alcohol-api/getdata', { usrid: 'usrid' })
+let datArr = []
 let loadData = () => {
-    axios.post(url + '/alcohol-api/getdata', { usrid: 'usrid' }).then((r) => {
-        r.data.data.map(x => {
-            let img;
-            console.log(x);
-            x.product_type == "บุหรี่" || x.product_type == "บุหรี" ? img = "cigarettes.png" : null;
-            x.product_type == "สุรา" || x.product_type == "เหล้า" ? img = "alcohol.png" : null;
-            x.product_type == "บุหรี่และสุรา" || x.product_type == "เหล้าและบุหรี่" ? img = "no-alcohol.png" : null;
-            document.getElementById("content").innerHTML += `<div class="inner-box mt-2" >
-                <div class="content" >
-                    <span class="company-logo"><img src="./../images/${img}" alt=""></span>
-                    <h4><span>${x.retail_name}</span></h4>
-                    <span>${x.owner_name}</span>
-                    <ul class="job-info">
-                        <li><i class="bi bi-cash-coin"></i> ${x.retail_type}</li>
-                        <li><i class="bi bi-cart3"></i> ${x.product_type}</li>
-                        <li><i class="bi bi-journal-check"></i> ${x.certification}ใบอนุญาต</li>
-                        <li><i class="bi bi-calendar-week"></i> ${x.ndate}</li>
-                    </ul>
-                </div>
-                <ul class="job-other-info ">
-                    <li class="f-16 privacy cursor" onclick="editData('${x.gid}')"><i class="bi bi-clipboard-data"></i> รายละเอียด</li>
-                    <li class="required cursor" onclick="deleteData('${x.gid}')"><i class="bi bi-trash"></i> ลบ</li>
-                </ul>
-            </div>`
-        })
+    datArr = []
+    xdata.then((r) => {
+        datArr.push(r.data.data)
+        showData(r.data.data)
     })
 }
 
+let showData = (objArr) => {
+    objArr.map(x => {
+        let img;
+        // console.log(x);
+        x.product_type == "บุหรี่" || x.product_type == "บุหรี" ? img = "cigarettes.png" : null;
+        x.product_type == "สุรา" || x.product_type == "เหล้า" ? img = "alcohol.png" : null;
+        x.product_type == "บุหรี่และสุรา" || x.product_type == "เหล้าและบุหรี่" ? img = "no-alcohol.png" : null;
+        document.getElementById("content").innerHTML += `<div class="inner-box mt-2" >
+            <div class="content" >
+                <span class="company-logo"><img src="./../images/${img}" alt=""></span>
+                <h4><span>${x.retail_name}</span></h4>
+                <span>${x.owner_name}</span>
+                <ul class="job-info">
+                    <li><i class="bi bi-cash-coin"></i> ${x.retail_type}</li>
+                    <li><i class="bi bi-cart3"></i> ${x.product_type}</li>
+                    <li><i class="bi bi-journal-check"></i> ${x.certification}ใบอนุญาต</li>
+                    <li><i class="bi bi-calendar-week"></i> ${x.ndate}</li>
+                </ul>
+            </div>
+            <ul class="job-other-info ">
+                <li class="f-16 privacy cursor" onclick="editData('${x.gid}')"><i class="bi bi-clipboard-data"></i> รายละเอียด</li>
+                <li class="required cursor" onclick="deleteData('${x.gid}')"><i class="bi bi-trash"></i> ลบ</li>
+            </ul>
+        </div>`
+    })
+}
 
+let findData = () => {
+    document.getElementById("content").innerHTML = "";
+    let find = document.getElementById('find').value;
+    let datFilter = datArr[0].filter(x => x.txt.includes(find))
+    console.log(datFilter);
+    showData(datFilter)
+}
 
 let deleteData = (gid) => {
     axios.post(url + '/alcohol-api/delete', { gid }).then(r => {
